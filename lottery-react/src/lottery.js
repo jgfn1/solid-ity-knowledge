6f063ca53bcfa0ef14f2909e0632833622c51091
jgfn1 <jgfn@cin.ufpe.br>
2023-01-26 11:45:09 -0300

    simple web app to interact with the lottery

---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,9 @@
+import web3 from './web3';
+
+const address = '0x8DC4E7fE440F7E766e52b887a2f9322FD182E2dd';
+
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+const lottery = new web3.eth.Contract(abi, address);
+
+export default lottery;
