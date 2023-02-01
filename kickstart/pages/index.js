2d18fea39150fad9e8a2ecf2ba8e154a0bf52bb5
jgfn1 <jgfn@cin.ufpe.br>
2023-02-01 11:21:45 -0300

    placing the css stylesheet tag at a proper location

--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -26,14 +26,14 @@ class CampaignIndex extends Component {
     return (
       <Layout>
         <div>
-          <link
-            async
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
-          />
           <h3>Open Campaigns</h3>
+          <Button
+            floated="right"
+            content="Create Campaign"
+            icon="add circle"
+            primary
+          ></Button>
           {this.renderCampaigns()}
-          <Button content="Create Campaign" icon="add circle" primary></Button>
         </div>
       </Layout>
     );
